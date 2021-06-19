0db74b4cb63e8158102887a87aa42d30a73713f7
joelcg <77513886+joelcg@users.noreply.github.com>
2021-06-19 11:04:02 +0700

    Update fibonacciSeq.py

--- fibonacciSeq.py
+++ fibonacciSeq.py
@@ -1,7 +1,7 @@
-def getFibNum(pos):
+def getFibNumRec(pos):
   if pos == 0 or pos == 1: return pos
-  return getFibNum(pos - 1) + getFibNum(pos - 2)
+  return getFibNumRec(pos - 1) + getFibNumRec(pos - 2)
 
-def getFibSeq(num):
+def getFibSeqRec(num):
   for num in range(num):
-    print(getFibNum(num))
+    print(getFibNumRec(num))
