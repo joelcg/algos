81b8d00d40c1d810debac8cb1b391d1455077e5c
joelcg <joseplois@protonmail.com>
2021-08-13 15:49:31 +0700

    add lines ver for fizzy

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,5 @@
-'use strict';
-
-function fizzbuzz(n)
-{
+# RETURN ARRAY
+function fizzbuzz(n) {
   var array = [];
   for (var i = 1; i <= n; i++) {
     let fizz = (i % 3 == 0),
@@ -16,4 +14,21 @@ function fizzbuzz(n)
     }
   }
   return array;
+}
+
+
+# RETURN OUTPUT ON EACH LINES
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let output = '',
+      fizz = (i % 3 == 0),
+      buzz = (i % 5 == 0);
+    if (fizz || buzz) {
+      (fizz? output += "Fizz" : "") + 
+      (buzz? output += "Buzz" : "")
+    } else { 
+      output = i 
+    }
+    console.log(output);
+  }
 }
